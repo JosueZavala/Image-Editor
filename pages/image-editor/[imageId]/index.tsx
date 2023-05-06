8567d413f61b45c14bb8c407fcdb39817ccbbad6
Josue Zavala <josue.zavala123@gmail.com>
2023-05-06 00:18:46 -0600

    Cleaning code

--- pages/image-editor/[imageId]/index.tsx
+++ pages/image-editor/[imageId]/index.tsx
@@ -1,19 +1,11 @@
 import ImageEditorContainer from "@/components/ImageEditor/ImageEditor";
-import ImageCard from "@/components/ImageSelector/ImageCard";
-import { ArrayImages, IMAGES_DOMAIN } from "@/data/constans";
-import Image from "next/image";
+import { ArrayImages } from "@/data/constans";
 
 export default function ImageEditor(props: any) {
   return (
     <main className="m-8 p-8">
       <h1 className="text-4xl p-4">Edit your Image</h1>
       <section className="grid grid-flow-col auto-cols-auto gap-4 p-4 bg-blue-200 w-fit">
-        {/* <div className="grid justify-items-center">
-          <ImageCard
-          image={`${IMAGES_DOMAIN}${props.imageId}`}
-          resolution={"800x600"}
-          />
-        </div> */}
         <ImageEditorContainer imageId={props.imageId} />
       </section>
     </main>
